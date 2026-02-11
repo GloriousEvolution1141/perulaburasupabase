048876e39a79aba19895342d5c960780e5131b94
GloriousEvolution1141 <adcoaquiraq@gmail.com>
2026-02-11 08:26:12 -0500

    clear template - botton logout & data

--- app/page.tsx
+++ app/page.tsx
@@ -34,18 +34,7 @@ export default function Home() {
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-4">
-          <p>
-            PeruLabura © 2026 - Impulsando el Empleo Local
-            {/* Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a> */}
-          </p>
+          <p>PeruLabura © 2026 - Impulsando el Empleo Local</p>
           {/* <ThemeSwitcher /> */}
         </footer>
       </div>
--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,6 +1,4 @@
 import { AuthButton } from "@/components/auth-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { hasEnvVars } from "@/lib/utils";
 import { Suspense } from "react";
 
 export default function ProtectedLayout({
@@ -19,13 +17,16 @@ export default function ProtectedLayout({
                 <DeployButton />
               </div> */}
             </div>
-            {!hasEnvVars ? (
+            {/* {!hasEnvVars ? (
               <EnvVarWarning />
             ) : (
               <Suspense>
                 <AuthButton />
               </Suspense>
-            )}
+            )} */}
+            <Suspense>
+              <AuthButton />
+            </Suspense>
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
