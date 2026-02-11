f1f907518af93e93c48981b78d62b3fa1feb8822
GloriousEvolution1141 <adcoaquiraq@gmail.com>
2026-02-11 08:08:56 -0500

    update : metadata & icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "PeruLabura",
+  description:
+    "Una plataforma dinámica donde las ofertas de trabajo tienen una vida útil limitada. Crea, gestiona y descubre oportunidades que desaparecen al expirar.",
 };
 
 const geistSans = Geist({
