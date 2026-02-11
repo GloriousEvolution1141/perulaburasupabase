8700fe0fb2f30161bfd91f31cb435ab71df298fc
GloriousEvolution1141 <adcoaquiraq@gmail.com>
2026-02-11 07:56:44 -0500

    clear template

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,9 +1,6 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
+import { EnvVarWarning } from "@/components/env-var-warning";
 import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default function ProtectedLayout({
@@ -17,10 +14,10 @@ export default function ProtectedLayout({
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+              {/* <Link href={"/"}>Next.js Supabase Starter</Link>
               <div className="flex items-center gap-2">
                 <DeployButton />
-              </div>
+              </div> */}
             </div>
             {!hasEnvVars ? (
               <EnvVarWarning />
@@ -35,9 +32,10 @@ export default function ProtectedLayout({
           {children}
         </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-4">
           <p>
-            Powered by{" "}
+            PeruLabura © 2026 - Impulsando el Empleo Local
+            {/* Powered by{" "}
             <a
               href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
               target="_blank"
@@ -45,9 +43,9 @@ export default function ProtectedLayout({
               rel="noreferrer"
             >
               Supabase
-            </a>
+            </a> */}
           </p>
-          <ThemeSwitcher />
+          {/* <ThemeSwitcher /> */}
         </footer>
       </div>
     </main>
