772d2cdade5b488308367e32fac8bffc8a65d5d7
GloriousEvolution1141 <adcoaquiraq@gmail.com>
2026-02-18 22:43:33 -0500

    update footer movil version

--- components/content/FooterMain.tsx
+++ components/content/FooterMain.tsx
@@ -1,7 +1,7 @@
 export default function FooterMain() {
   return (
     <>
-      <footer className="fixed bottom-0 left-0 w-full flex items-center justify-center border-t bg-white text-center text-xs py-4">
+      <footer className="w-full flex items-center justify-center border-t bg-white text-center text-xs py-4 md:fixed md:bottom-0">
         <p>TacnaLabura © 2026 - Impulsando el Empleo Local</p>
       </footer>
     </>
